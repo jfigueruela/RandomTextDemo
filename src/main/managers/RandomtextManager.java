bc73a0366c7ef11763b202393c3a2410f7cfab4b
Jose Figueruela <jose.figueruela@codebusters.es>
2016-06-26 20:30:51 +0200

    Initial import

---
+++ src/main/managers/RandomtextManager.java
@@ -0,0 +1,36 @@
+package managers;
+
+import java.util.List;
+
+import dtos.DemoDTO;
+import services.RandomtextService;
+import utils.DemoUtils;
+import utils.RandomtextTextRequest;
+import utils.RandomtextTextResponse;
+
+public class RandomtextManager {
+
+    /**
+     * 
+     * @param pStart
+     * @param pEnd
+     * @param wCountMin
+     * @param wCountMax
+     * @return
+     */
+    public static DemoDTO process(Integer pStart, Integer pEnd,
+            Integer wCountMin, Integer wCountMax) {
+        DemoDTO result = new DemoDTO();
+
+        RandomtextService randomtextService = new RandomtextService();
+        // Number of paragraphs, generated random value.
+        int paragrapthNumber = DemoUtils.getRandomValue(pStart, pEnd);
+        List<RandomtextTextRequest> list = randomtextService
+                .getRandomTextList(paragrapthNumber);
+
+        RandomtextTextResponse randomtextTextResponse = new RandomtextTextResponse(
+                list.get(0).getText_out(), 2, 3, 4);
+
+        return result;
+    }
+}
